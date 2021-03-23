213e0154ae3eadf6fcecf062329185b293006c52
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-03-23 17:07:10 -0600

    add behaviour to forgot-psw component

--- src/app/modules/auth/pages/forgot-psw/forgot-psw.component.ts
+++ src/app/modules/auth/pages/forgot-psw/forgot-psw.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator } from 'src/app/tools/validators/email.validator';
 
 @Component({
   selector: 'app-forgot-psw',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-psw.component.scss']
 })
 export class ForgotPswComponent implements OnInit {
+  usermail: string;
+  email: AbstractControl;
 
-  constructor() { }
+  form: FormGroup;
+  reqStatus: number = 0;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      email: [
+        '',
+        Validators.compose([Validators.required, EmailValidator.validate])
+      ]
+    });
+
+    this.email = this.form.controls['email'];
   }
 
+  sendPswResetEmail(email) {
+  }
 }
