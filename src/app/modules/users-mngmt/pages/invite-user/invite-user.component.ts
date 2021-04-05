4ad08e4ff4143d226de228a584259cf3540565e7
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-04-05 11:48:31 -0500

    show options based on role selecion

--- src/app/modules/users-mngmt/pages/invite-user/invite-user.component.ts
+++ src/app/modules/users-mngmt/pages/invite-user/invite-user.component.ts
@@ -13,8 +13,102 @@ export class InviteUserComponent implements OnInit {
 
   form: FormGroup;
   email: AbstractControl;
-  roles: any = ['Administrador', 'País', 'Retail', 'HP'];
-  countries = ['Argentina', 'Chile', 'Colombia', 'México'];
+  roles: any = [
+    {
+      id: 1,
+      name: 'admin'
+    },
+    {
+      id: 2,
+      name: 'country'
+    },
+    {
+      id: 3,
+      name: 'retailer'
+    },
+    {
+      id: 4,
+      name: 'hp'
+    }
+  ];
+  countries = [
+    {
+      id: 1,
+      name: 'México'
+    },
+    {
+      id: 2,
+      name: 'Argentina'
+    },
+    {
+      id: 3,
+      name: 'Chile'
+    },
+    {
+      id: 4,
+      name: 'Colombia'
+    }
+  ];
+  retailers = [
+    {
+      id: 1,
+      name: 'Liverpool'
+    },
+    {
+      id: 2,
+      name: 'Carrefourl'
+    },
+    {
+      id: 3,
+      name: 'Frávega'
+    },
+    {
+      id: 4,
+      name: 'Garbarino'
+    },
+    {
+      id: 5,
+      name: 'Office Depot'
+    },
+    {
+      id: 6,
+      name: 'Office Max'
+    },
+  ];
+  sectors = [
+    {
+      id: 1,
+      name: 'Marketing'
+    },
+    {
+      id: 2,
+      name: 'Retail'
+    },
+    {
+      id: 3,
+      name: 'WWS PS'
+    },
+    {
+      id: 4,
+      name: 'WWS Print'
+    }
+  ];
+
+  categories = [
+    {
+      id: 1,
+      name: 'Cómputo'
+    },
+    {
+      id: 2,
+      name: 'Impresoras'
+    },
+    {
+      id: 3,
+      name: 'Suministros'
+    }
+  ]
+  selectedRole: any;
 
   constructor(private fb: FormBuilder) { }
 
@@ -36,9 +130,37 @@ export class InviteUserComponent implements OnInit {
         this.countries.map(() => this.fb.control('')),
         MultipleCheckboxValidator.validate
       ),
+      retailers: this.fb.array(
+        this.retailers.map(() => this.fb.control('')),
+        MultipleCheckboxValidator.validate
+      ),
+      sectors: this.fb.array(
+        this.sectors.map(() => this.fb.control('')),
+        MultipleCheckboxValidator.validate
+      ),
+      categories: this.fb.array(
+        this.categories.map(() => this.fb.control('')),
+        MultipleCheckboxValidator.validate
+      ),
     });
 
     this.email = this.form.controls['email'];
+  }
+
+  roleChange() {
+    if (this.selectedRole) {
+      console.log('exists selectedRole', this.selectedRole)
+      this.resetCheckboxes();
+    }
+  }
+
+  resetCheckboxes() {
+    this.form.reset({
+      'countries': '',
+      'retailers': '',
+      'sectors': '',
+      'categories': ''
+    });
   }
 
   convertToValue(key: string) {
