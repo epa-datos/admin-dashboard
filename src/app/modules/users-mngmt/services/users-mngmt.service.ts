bb1c324f4028f09bd18254c036902b4ff26a254d
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-04-07 17:17:11 -0500

    add send invitation endpoint to users-mngmnt service

--- src/app/modules/users-mngmt/services/users-mngmt.service.ts
+++ src/app/modules/users-mngmt/services/users-mngmt.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { Configuration } from 'src/app/app.constants';
+import { Invite } from 'src/app/models/permission';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,9 @@ export class UsersMngmtService {
     this.baseUrl = this.config.endpoint;
   }
 
+  /**** USERS MANAGMENT
+  * Endpoints related to list and remove created users
+  * ****/
   getUsers() {
     return this.http.get(`${this.baseUrl}/users`);
   }
@@ -27,6 +31,9 @@ export class UsersMngmtService {
     return this.http.delete(`${this.baseUrl}/users/${userID}`);
   }
 
+  /**** ROLE AND ENTITIES
+  * Endpoints related to role and entities to invite a new user
+  * ****/
   getRoles() {
     return this.http.get(`${this.baseUrl}/roles`);
   }
@@ -45,5 +52,15 @@ export class UsersMngmtService {
 
   getCategories() {
     return this.http.get(`${this.baseUrl}/categories`);
+  }
+
+  /**** INVITATIONS
+  * Endpoints related to user invitation
+  * ****/
+  sendInvitation(invite: Invite) {
+    if (!invite) {
+      return throwError('[users-mngmt.service]: not invite provided');
+    }
+    return this.http.post(`${this.baseUrl}/users/invitations`, invite);
   }
 }
