c1bc48a5dae3b836ba1fe0b8a58926ad216415aa
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-04-13 18:10:03 -0500

    add dashboard component

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 import { CountryComponent } from './pages/country/country.component';
 import { RetailerComponent } from './pages/retailer/retailer.component';
-import { RouterModule } from '@angular/router';
 import { DashboardRoutes } from './dashboard.routing';
 
 
 
 @NgModule({
-  declarations: [CountryComponent, RetailerComponent],
+  declarations: [
+    DashboardComponent,
+    CountryComponent,
+    RetailerComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(DashboardRoutes),
--- src/app/modules/dashboard/dashboard.routing.ts
+++ src/app/modules/dashboard/dashboard.routing.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 
-import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { InvestmentComponent } from '../../pages/investment/investment.component';
 import { CountryComponent } from './pages/country/country.component';
 import { RetailerComponent } from './pages/retailer/retailer.component';
 
 export const DashboardRoutes: Routes = [
-    { path: 'investment', component: DashboardComponent },
     { path: 'country', component: CountryComponent },
     { path: 'retailer', component: RetailerComponent },
 ];
