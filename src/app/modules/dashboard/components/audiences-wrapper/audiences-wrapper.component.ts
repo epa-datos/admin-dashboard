30bfee6a47a3a19c10aae663605a19522c0c374f
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-04-15 17:43:20 -0500

    use graph-line component in audiences-wrapper component

--- src/app/modules/dashboard/components/audiences-wrapper/audiences-wrapper.component.ts
+++ src/app/modules/dashboard/components/audiences-wrapper/audiences-wrapper.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AudiencesWrapperComponent implements OnInit {
 
+  trafficByMonths = []
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getTrafficByMonths();
+  }
+
+  getTrafficByMonths() {
+    let firstDate = new Date();
+    firstDate.setDate(firstDate.getDate() - 90);
+    let visits = 1200;
+    for (var i = 0; i < 90; i++) {
+      let newDate = new Date(firstDate);
+      newDate.setDate(newDate.getDate() + i);
+
+      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+
+      this.trafficByMonths.push({
+        date: newDate,
+        visits: visits
+      });
+    }
   }
 
 }
