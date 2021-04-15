6a3a098bf3220503eb4ee9459da4ae0b4eefa580
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-04-14 23:41:43 -0500

    use graph-line-series component in retailer component

--- src/app/modules/dashboard/pages/retailer/retailer.component.ts
+++ src/app/modules/dashboard/pages/retailer/retailer.component.ts
@@ -12,6 +12,49 @@ export class RetailerComponent implements OnInit {
   retailerName;
   activeTabView: number = 1;
 
+  campMetrics = [
+    {
+      name: 'Clicks',
+      serie: [
+        { date: new Date(2021, 1, 1), value: 25000 },
+        { date: new Date(2021, 2, 1), value: 47000 },
+        { date: new Date(2021, 3, 1), value: 50000 }
+      ]
+    },
+    {
+      name: 'Impresiones',
+      serie: [
+        { date: new Date(2021, 1, 1), value: 20000 },
+        { date: new Date(2021, 2, 1), value: 35000 },
+        { date: new Date(2021, 3, 1), value: 40000 }
+      ]
+    },
+    {
+      name: 'CPC',
+      serie: [
+        { date: new Date(2021, 1, 1), value: 45000 },
+        { date: new Date(2021, 2, 1), value: 37000 },
+        { date: new Date(2021, 3, 1), value: 40000 }
+      ]
+    },
+    {
+      name: 'Inversión',
+      serie: [
+        { date: new Date(2021, 1, 1), value: 30000 },
+        { date: new Date(2021, 2, 1), value: 27510 },
+        { date: new Date(2021, 3, 1), value: 50000 }
+      ]
+    },
+    {
+      name: 'Inversión',
+      serie: [
+        { date: new Date(2021, 1, 1), value: 30000 },
+        { date: new Date(2021, 2, 1), value: 27510 },
+        { date: new Date(2021, 3, 1), value: 50000 }
+      ]
+    }
+  ]
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
