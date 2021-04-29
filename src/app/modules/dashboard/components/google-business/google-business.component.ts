2d2a36ac41c48f7b550ba57b0330748b24ddda47
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-04-29 11:14:16 -0500

    use chart-multiple-axes component in google-business component

--- src/app/modules/dashboard/components/google-business/google-business.component.ts
+++ src/app/modules/dashboard/components/google-business/google-business.component.ts
@@ -18,30 +18,52 @@ export class GoogleBusinessComponent implements OnInit, AfterViewInit {
   cityList: any[] = [{ id: 1, name: 'Ciudad 1' }, { id: 2, name: 'Ciudad 2' }, { id: 3, name: 'Ciudad 3' }, { id: 4, name: 'Ciudad 4' }, { id: 5, name: 'Ciudad 5' }, { id: 3, name: 'Ciudad 6' }];
   constructor() { }
 
-  visitsVsRate = [
-    { date: moment(new Date(2021, 3, 15)).format('MMM DD'), value1: 1200, value2: 200 },
-    { date: moment(new Date(2021, 3, 16)).format('MMM DD'), value1: 1600, value2: 230 },
-    { date: moment(new Date(2021, 3, 17)).format('MMM DD'), value1: 1400, value2: 180 },
-    { date: moment(new Date(2021, 3, 18)).format('MMM DD'), value1: 1250, value2: 80 },
-    { date: moment(new Date(2021, 3, 19)).format('MMM DD'), value1: 800, value2: 60 },
-    { date: moment(new Date(2021, 3, 20)).format('MMM DD'), value1: 1000, value2: 110 },
-    { date: moment(new Date(2021, 3, 21)).format('MMM DD'), value1: 1100, value2: 120 }
-  ]
+  visitsVsRate = [{
+    'date': '2021-03-15',
+    'visits': 1298,
+    'visits_rate': 0.68,
+  }, {
+    'date': '2021-03-16',
+    'visits': 816,
+    'visits_rate': 0.71,
+  }, {
+    'date': '2021-03-17',
+    'visits': 1963,
+    'visits_rate': 0.7,
+  }, {
+    'date': '2021-03-18',
+    'visits': 1809,
+    'visits_rate': 0.83,
+  }, {
+    'date': '2021-03-19',
+    'visits': 1434,
+    'visits_rate': 0.8,
+  }, {
+    'date': '2021-03-20',
+    'visits': 2359,
+    'visits_rate': 0.91,
+  }, {
+    'date': '2021-03-21',
+    'visits': 2114,
+    'visits_rate': 1,
+  },];
 
   displayedColumns: string[] = ['province', 'city', 'store', 'visits', 'visitRate'];
+  displayedColumns2: string[] = ['province', 'city', 'store', 'visits', 'visitRate', 'storeVisitsRate'];
   private categories = [
-    { province: 'Buenos Aires', city: 'Moreno', store: 'Francisco Narciso de Laprida 386', visits: 250, visitRate: 14.45 },
-    { province: 'Ciudad autonoma de buenos Aires', city: 'Flores', store: 'Laprida 394', visits: 114, visitRate: 14.85 },
-    { province: 'Córdoba', city: 'Córdoba', store: 'Avenida Cabildo 2075', visits: 150, visitRate: 13.14 },
-    { province: 'Buenos Aires', city: 'Castelar', store: 'Hipolito Yrigoyen 769 (Ex ruta 197)', visits: 130, visitRate: 14.12 },
-    { province: 'Mendoza', city: 'Ciudad Autonoma de Buenos Aires', store: 'Belgrano', visits: 118, visitRate: 14.10 },
-    { province: 'Ciudad autonoma de buenos Aires', city: 'Buenos Aires', store: 'Av Gral. Fernández de la Cruz 4602', visits: 195, visitRate: 8.85 },
-    { province: 'Ciudad autonoma de buenos Aires', city: 'Belgrano', store: 'Avenida Rivadavia 5249', visits: 180, visitRate: 12.26 },
+    { province: 'Buenos Aires', city: 'Moreno', store: 'Francisco Narciso de Laprida 386', visits: 250, visitRate: 14.45, storeVisitsRate: 12.12 },
+    { province: 'Ciudad autonoma de buenos Aires', city: 'Flores', store: 'Laprida 394', visits: 114, visitRate: 14.85, storeVisitsRate: 14.05 },
+    { province: 'Córdoba', city: 'Córdoba', store: 'Avenida Cabildo 2075', visits: 150, visitRate: 13.14, storeVisitsRate: 13.01 },
+    { province: 'Buenos Aires', city: 'Castelar', store: 'Hipolito Yrigoyen 769 (Ex ruta 197)', visits: 130, visitRate: 11.25, storeVisitsRate: 14.11 },
+    { province: 'Mendoza', city: 'Ciudad Autonoma de Buenos Aires', store: 'Belgrano', visits: 118, visitRate: 14.10, storeVisitsRate: 13.12 },
+    { province: 'Ciudad autonoma de buenos Aires', city: 'Buenos Aires', store: 'Av Gral. Fernández de la Cruz 4602', visits: 195, visitRate: 8.85, storeVisitsRate: 8.32 },
+    { province: 'Ciudad autonoma de buenos Aires', city: 'Belgrano', store: 'Avenida Rivadavia 5249', visits: 180, visitRate: 12.26, storeVisitsRate: 9.45 },
   ]
   dataSource = new MatTableDataSource<any>(this.categories);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
+  selectedTab1 = 1;
 
   ngOnInit(): void {
   }
