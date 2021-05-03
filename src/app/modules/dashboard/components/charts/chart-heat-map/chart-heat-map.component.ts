e87b0c7bf7fd4b018dfc529d854858776211bc66
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-05-03 09:52:11 -0500

    update colors and borders in chart-heap-map instances from overview-wrapper component

--- src/app/modules/dashboard/components/charts/chart-heat-map/chart-heat-map.component.ts
+++ src/app/modules/dashboard/components/charts/chart-heat-map/chart-heat-map.component.ts
@@ -82,16 +82,17 @@ export class ChartHeatMapComponent implements OnInit, AfterViewInit {
     series.defaultState.transitionDuration = 3000;
 
     let bgColor = new am4core.InterfaceColorSet().getFor('background');
-    let bgColor2 = am4core.color('#adb5bd');
 
     let columnTemplate = series.columns.template;
-    columnTemplate.strokeWidth = 1;
-    columnTemplate.strokeOpacity = 0.2;
 
     if (this.showGridBorders) {
+      let bgColor2 = am4core.color('#ffffff');
       columnTemplate.stroke = bgColor2;
+      columnTemplate.strokeWidth = 2;
     } else {
       columnTemplate.stroke = bgColor
+      columnTemplate.strokeWidth = 1;
+      columnTemplate.strokeOpacity = 0.2;
     }
 
     let tooltipLegend = this.showTooltipValue
