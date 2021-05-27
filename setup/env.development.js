c1bf4e11822f9b4c1ae2b74e537a492b3f7516eb
María Isabel Álvarez Hernández <maria.alvarez9409@gmail.com>
2021-05-26 20:18:25 -0500

    setup app to serve it with app.js file (#115)
    * add express dependencies to package.json
    
    * add env files in setup dir
    
    * add app.js to serve the app
    
    * add .package_prod.json file
    
    * move corse to dependencies in package.json
    
    * update apiUrl in environment variables

---
+++ setup/env.development.js
@@ -0,0 +1,6 @@
+// Development Enviroment variables;
+(function (window) {
+  window.__env = window.__env || {};
+  window.__env.endpoint = 'https://bdd-epa-digital.uc.r.appspot.com/api/v1';
+  window.__env.production = false;
+}(this));
---
+++ setup/env.production.js
@@ -0,0 +1,7 @@
+// Production Enviroment variables
+(function (window) {
+  window.__env = window.__env || {};
+  window.__env.endpoint = 'https://bdd-epa-digital.uc.r.appspot.com/api/v1';
+  window.__env.production = true;
+
+}(this));
