2105cd2200303bb86ad6ea49af62fde742c25c62
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-05-27 18:44:50 -0500

    update methods to load custom titles in navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,6 +4,8 @@ import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user';
 import { AppStateService } from 'src/app/services/app-state.service';
 import { Subscription } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -21,13 +23,25 @@ export class NavbarComponent implements OnInit, OnDestroy {
   currentRoute;
 
   sidebarSub: Subscription;
+  routeSub: Subscription;
+
+  mainRegionSub: Subscription;
   countrySub: Subscription;
   retailerSub: Subscription;
+
+  newMainRegion;
+  newCountry;
+  newRetailer;
+
+  mainRegionInit: boolean = true;
+  countryInit: boolean = true;
+  retailerInit: boolean = true;
 
   constructor(
     location: Location,
     private userService: UserService,
-    private appStateService: AppStateService
+    private appStateService: AppStateService,
+    private router: Router,
   ) {
     this.location = location;
   }
@@ -35,42 +49,78 @@ export class NavbarComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.user = this.userService.user;
 
-    if (this.appStateService.selectedCountry) {
-      this.customTitle = this.appStateService.selectedCountry.name;
-    }
-    if (this.appStateService.selectedRetailer && this.user.role_name !== 'retailer') {
-      this.customSubtitle = this.appStateService.selectedRetailer.name;
-    } else if (this.appStateService.selectedRetailer) {
-      this.customTitle = this.appStateService.selectedRetailer.name;
-    }
+    this.newRetailer = this.appStateService.selectedRetailer;
+    this.newCountry = this.appStateService.selectedCountry;
+    this.newMainRegion = this.appStateService.selectedMainRegion;
 
     // sidebar titles
     this.sidebarSub = this.appStateService.sidebarData$.subscribe(resp => {
       this.routes = resp;
       this.listTitles = this.routes.filter(listTitle => listTitle);
+      this.loadCustomTitles(this.router.url);
     }, error => {
       console.error(`[navbar.component]: ${error}`);
     })
 
     // custom title
+    this.mainRegionSub = this.appStateService.selectedMainRegion$.subscribe(resp => {
+      this.newMainRegion = resp;
+      this.mainRegionInit && this.loadCustomTitles(this.router.url);
+      this.mainRegionInit = false;
+    }, error => {
+      console.error(`[navbar.component]: ${error}`);
+    });
+
+    // custom title
     this.countrySub = this.appStateService.selectedCountry$.subscribe(resp => {
-      this.customTitle = resp?.name ? resp.name : undefined;
-      // this.customizeTitle();
+      this.newCountry = resp;
+      this.countryInit && this.loadCustomTitles(this.router.url);
+      this.countryInit = false;
     }, error => {
       console.error(`[navbar.component]: ${error}`);
     });
 
     // custom subtitle
     this.retailerSub = this.appStateService.selectedRetailer$.subscribe(resp => {
-      if (this.userService.user.role_name === 'retailer') {
-        this.customTitle = resp?.name ? resp?.name : undefined;
-      } else {
-        this.customSubtitle = resp?.name ? resp.name : undefined;
-      }
-      // this.customizeTitle();
+      this.newRetailer = resp;
+      this.retailerInit && this.loadCustomTitles(this.router.url);
+      this.retailerInit = false;
     }, error => {
       console.error(`[navbar.component]: ${error}`);
     });
+
+    this.routeSub = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd))
+      .subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          this.loadCustomTitles(event.url);
+        }
+      });
+  }
+
+  loadCustomTitles(currentUrl: string) {
+    const newMenuItem = this.listTitles.find(title => title.path === currentUrl);
+
+    if (newMenuItem) {
+      // applies for simple options of menu (without submenu property)
+      this.customTitle = newMenuItem.title;
+      this.customSubtitle && delete this.customSubtitle;
+    } else {
+      if (this.newRetailer?.id) {
+        if (this.userService.user.role_name === 'retailer') {
+          this.customTitle = this.newRetailer.name;
+        } else {
+          this.customTitle = this.newCountry.name;
+          this.customSubtitle = this.newRetailer.name;
+        }
+      } else if (this.newCountry?.id) {
+        this.customTitle = this.newCountry.name;
+        this.customSubtitle && delete this.customSubtitle;
+      } else if (this.newMainRegion?.name) {
+        this.customTitle = this.newMainRegion.name;
+        this.customSubtitle && delete this.customSubtitle;
+      }
+    }
   }
 
   getTitleByRoute() {
@@ -105,7 +155,9 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.sidebarSub?.unsubscribe();
+    this.mainRegionSub?.unsubscribe();
     this.countrySub?.unsubscribe();
     this.retailerSub?.unsubscribe();
+    this.routeSub?.unsubscribe();
   }
 }
