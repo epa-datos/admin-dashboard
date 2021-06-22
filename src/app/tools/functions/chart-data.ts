a6028f1ef2b5f6bde61e76c6d1208a062294de9c
MaryAlvarezH <maria.alvarez9409@gmail.com>
2021-06-22 12:45:58 -0500

    update disaggregatePictorialData function in chart-data.ts

--- src/app/tools/functions/chart-data.ts
+++ src/app/tools/functions/chart-data.ts
@@ -3,26 +3,39 @@
  * @param metricName1 name property (value) to to distinguish the first metric
  * @param metricName2 name property (value) to to distinguish the second metric
  * @param dataRaw array with two metrics (objects)
+ * @param usePercentage by default returns value property in percentage otherwise use original value
  * @returns an object with 2 objetcs (disaggregated metric1 and metric2 data)
  */
-export function disaggregatePictorialData(metricName1: string, metricName2: string, dataRaw: any[]): {} {
+
+export function disaggregatePictorialData(metricName1: string, metricName2: string, dataRaw: any[], usePercentage: boolean = true): {} {
     let disMetric1;
     let disMetric2;
 
     const metric1 = dataRaw.find(item => item.name === metricName1);
     const metric2 = dataRaw.find(item => item.name === metricName2);
 
-    let devicePerc = getPercentageValues(metric1?.value, metric2?.value);
+    let value1: any;
+    let value2: any;
+
+    if (usePercentage) {
+        const percValues = getPercentageValues(metric1?.value, metric2?.value);
+        value1 = percValues.perc1;
+        value2 = percValues.perc2;
+    } else {
+        value1 = metric1.value.toFixed(2);
+        value2 = metric2.value.toFixed(2);
+    }
 
     if (metric1) {
         disMetric1 = [
             {
                 name: 'empty',
-                value: devicePerc.perc1 ? +(100 - (+devicePerc.perc1)).toFixed(2) : 100
+                value: value1 ? +(100 - (+value1)).toFixed(2) : 100
             },
             {
                 name: metricName1,
-                value: devicePerc.perc1 ? +devicePerc.perc1 : 0, rawValue: metric1.value
+                value: value1 ? +value1 : 0,
+                rawValue: usePercentage ? metric1.value.toFixed(2) : undefined
             },
         ];
     } else {
@@ -33,18 +46,17 @@ export function disaggregatePictorialData(metricName1: string, metricName2: stri
         disMetric2 = [
             {
                 name: 'empty',
-                value: devicePerc.perc2 ? +(100 - (+devicePerc.perc2)).toFixed(2) : 100
+                value: value2 ? +(100 - (+value2)).toFixed(2) : 100
             },
             {
                 name: metricName2,
-                value: devicePerc.perc2 ? +devicePerc.perc2 : 0, rawValue: metric2.value
+                value: value2 ? +value2 : 0,
+                rawValue: usePercentage ? metric2.value.toFixed(2) : undefined
             },
         ];
     } else {
         disMetric2 = [];
     }
-
-    console.log('res',)
 
     return {
         [metricName1.toLowerCase()]: disMetric1,
